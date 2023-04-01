57c43bbb5136aa92277b5701b72c51a047e82f9f
amorilla42 <andresmm4242@gmail.com>
2023-04-01 21:11:17 +0200

    keep trying to exceute programs

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amorilla <amorilla@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 18:49:29 by amorilla          #+#    #+#             */
-/*   Updated: 2023/04/01 19:56:39 by amorilla         ###   ########.fr       */
+/*   Updated: 2023/04/01 20:39:53 by amorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ typedef struct s_instruction
 } t_instruction;
 
 t_instruction	*parse(char *str);
-int				execute(t_instruction *instr, char **path);
-void			loop_shell(char **path);
+int				execute(t_instruction *instr, char **path, char **env);
+void			loop_shell(char **path, char **env);
 void			free_path(char **path);
 char			**get_path(char **env);
 char			*check_access(char *command, char **path);
--- src/execute.c
+++ src/execute.c
@@ -6,13 +6,18 @@
 /*   By: amorilla <amorilla@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/31 20:10:42 by amorilla          #+#    #+#             */
-/*   Updated: 2023/04/01 19:57:03 by amorilla         ###   ########.fr       */
+/*   Updated: 2023/04/01 20:51:26 by amorilla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	execute_generic(t_instruction *instr)
+static int	execaux(char *comand, char **path, char ** env)
+{
+	
+}
+
+static int	execute_generic(t_instruction *instr, char **path, char **env)
 {
 	int		pid1;
 	int		result_code;
@@ -21,22 +26,25 @@ static int	execute_generic(t_instruction *instr)
 	if (pid1 < 0)
 		exit(pid1);
 	if (pid1 == 0)
-		
+	{
+		result_code = execaux(instr->instruction, path, env);
+		perror(strerror(result_code));
+	}
 	waitpid(pid1, 0, 0);
 	if (WIFEXITED(result_code))
 		exit(WEXITSTATUS(result_code));
 }
 
 
-int	execute(t_instruction *instr, char **path)
+int	execute(t_instruction *instr, char **path, char **env)
 {
 	int	status;
 
-	status = 1;
+	status = 127;
 	if (instr->type == CD)
 		status = chdir(instr->directory);
 	else if (instr->type == GENERIC)
-		status = execute_generic(instr);
+		status = execute_generic(instr, path, env);
 	return (status);
 }
 
