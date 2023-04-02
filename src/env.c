bf0f151fc6b146b0ab9754a6f5ded359da777ad3
Alejandro jose Ruzafa casas <aruzafa-@c3r4s4.42malaga.com>
2023-04-02 17:01:55 +0200

    Add env file

---
+++ src/env.c
@@ -0,0 +1,38 @@
+#include "minishell.h"
+
+static char	**free_args(char **arg)
+{
+	if (!arg)
+		return (0);
+	if (arg[0] != 0)
+		free(arg[0]);
+	if (arg[1] != 0)
+		free(arg[1]);
+	free(arg);
+	return (0);
+}
+
+char	*get_env_value(char **env, char *key)
+{
+	char	**arg;
+	char	**res;
+	size_t	key_len;
+
+	key_len = ft_strlen(key);
+	while (env)
+	{
+		arg = ft_split(*env, '=');
+		if (!arg || !arg[0] || !arg[1])
+			return (free_args(arg));
+		if (ft_strncmp(arg[0], key, key_len) == 0)
+		{
+			res = ft_strdup(arg[1]);
+			free_args(arg);
+			return (res);
+		}
+		else
+			free_args(arg);
+		env++;
+	}
+	return (0);
+}
