7dc504ebac4a1a7538082fc7a365954be70cbc75
Alejandro Ruzafa <ruzafa-@outlook.es>
2023-04-07 13:16:49 +0200

    Update libft to 1.0.1

--- libft/ft.h
+++ libft/ft.h
@@ -15,7 +15,25 @@
 
 # include "ft/libft.h"
 
+/**
+  * Lee una línea del fd proporcionado hasta llegar al salto de línea.
+  * @param fd El file descriptor del que se va a leer.
+  * @returns El string reservado con la línea incluyendo el saltn de línea
+  * o NULL en caso de error.
+  * En caso de que hayamos llegado al final del archivo, devolvemos
+  * el string sin salto de línea.
+  */
 char	*ft_get_next_line(int fd);
+
+/**
+  * Imprime la línea con el formato especificado:
+  * Los formatos implementados son los siguientes: cspdiuxX%
+  * Los flags implementados son los siguentes: -0.# + y el ancho mínimo.
+  * (Sí, uno de ellos es el espacio)
+  * @param format el string con un formato
+  * @param valores Por cada %formato, pasar su valor en una variable (variádicas)
+  * @returns el nº de caracteres impreso.
+  */
 int		ft_printf(char const *format, ...);
 
 #endif
