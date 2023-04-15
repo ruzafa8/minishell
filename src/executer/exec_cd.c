5cd40076839ddcc0db82f4eda308166f3c95c77d
amorilla42 <andresmm4242@gmail.com>
2023-04-15 21:15:08 +0200

    s+exec cd ma o meno

--- src/executer/exec_cd.c
+++ src/executer/exec_cd.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-int	built_in_cd(t_command *command, t_shell_data *sh_data)
+int	built_in_cd(t_command *command, t_shell_data *data)
 {
 	int	res_code;
 	char *pathaux;
@@ -8,10 +8,12 @@ int	built_in_cd(t_command *command, t_shell_data *sh_data)
 	if (command->argc == 1 
 		|| ft_strncmp(command->argv[1], "--", 3) == 0)
 	{
-		pathaux = get_env_value(sh_data, "HOME");
+		pathaux = get_env_value(data, "HOME");
 	//	if (!pathaux)
 	//		return (errorcomand(con la info));
 		res_code = chdir(pathaux);
+		//EL SET ENV VAR SE RECOYE
+		set_env_var(data, "y", "y");
 		//update_pwds() cambiar el pwd y el OLDpwd en el env a los nuevos
 	}
 	else
