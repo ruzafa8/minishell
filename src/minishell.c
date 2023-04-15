30c8e175ad6789f8e476a47151860cc5aa5bbc35
amorilla42 <andresmm4242@gmail.com>
2023-04-15 20:24:49 +0200

    Auto stash before merge of "10-parser" and "origin/10-parser"

--- src/minishell.c
+++ src/minishell.c
@@ -4,11 +4,9 @@ int	main(int argc, char **args, char **env)
 {
 	(void)argc;
 	(void)args;
-	char **path;
 	t_shell_data	*shell_data;
 
 	shell_data = init_shell_data(env);
-	path = get_path(env);
 	loop_shell(shell_data);
 	return (0);
 }
