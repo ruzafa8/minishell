2c72f786c3283c98a40e1f35742b1f222065c7fd
amorilla42 <andresmm4242@gmail.com>
2023-04-21 12:47:26 +0200

    add cases

--- src/executer/exec_cd.c
+++ src/executer/exec_cd.c
@@ -1,5 +1,32 @@
 #include "minishell.h"
 
+
+/**
+ * change old pwd and new pwd with the one at the parameter 
+ * if the "new" parameter is null, then swap oldpwd and pwd
+*/
+static void	update_pwds(t_shell_data *data, char *new)
+{
+	char	*aux;
+
+	if(new[0] == '\0') //caso de swap cuando hagas "cd -" o cosas asi
+	{
+		aux = ft_strdup(get_env_value(data, "OLDPWD"));
+		set_env_var(data, "OLDPWD", get_env_value(data, "PWD"));
+		set_env_var(data, "PWD", aux);
+		free(aux);
+	}
+	else //caso de cd normal
+	{
+		set_env_var(data, "OLDPWD", get_env_value(data, "PWD"));
+		set_env_var(data, "PWD", new);
+	}
+}
+/*
+	The target might not exist, 
+	the target might not be a directory, 
+	you might not have permission to access the target directory
+*/
 int	built_in_cd(t_command *command, t_shell_data *data)
 {
 	int	res_code;
@@ -9,12 +36,16 @@ int	built_in_cd(t_command *command, t_shell_data *data)
 		|| ft_strncmp(command->argv[1], "--", 3) == 0)
 	{
 		pathaux = get_env_value(data, "HOME");
-	//	if (!pathaux)
-	//		return (errorcomand(con la info));
+		
+		//if (!pathaux)
+		//	return (errorcomand(con la info));
+		
+		
 		res_code = chdir(pathaux);
-		//EL SET ENV VAR SE RECOYE
-		set_env_var(data, "y", "y");
-		//update_pwds() cambiar el pwd y el OLDpwd en el env a los nuevos
+		if(res_code == 0) //si el cd va todo perfectisimo
+			update_pwds(data, pathaux);
+		
+		free(pathaux);
 	}
 	else
 		res_code = chdir(command->argv[1]);
