6a22ecb49362f8553381e7abeb53c2cc4fde1cda
amorilla42 <andresmm4242@gmail.com>
2023-04-23 17:12:09 +0200

    exit built in

--- src/executer/execute.c
+++ src/executer/execute.c
@@ -77,6 +77,8 @@ int	execute(t_list *instr, t_shell_data *data)
 		status = built_in_cd(command, data);
 	else if (ft_strncmp(command->argv[0], "env", 4) == 0)
 		status = debug_env(data);//status = built_in_env(command, data);
+	else if (ft_strncmp(command->argv[0], "exit", 5) == 0)
+		exit(0);
 	else
 		status = execute_generic(command, data);
 /*
