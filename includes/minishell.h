21d44b8c5ff52d202b63f461f3c99a6697d9301e
amorilla42 <andresmm4242@gmail.com>
2023-04-28 13:25:00 +0200

    libft now pass norminette

--- includes/minishell.h
+++ includes/minishell.h
@@ -31,13 +31,13 @@ typedef struct s_shell_data
 	//pid_t		pid;
 }	t_shell_data;
 
-int			execute(t_list *instr, t_shell_data *data);
-void		loop_shell(t_shell_data *data);
-void		free_path(char **path);
-char		**get_path(char **env);
-char		*check_access(char *command, char **path);
-char		*get_env_value(t_shell_data *data, char *key);
-int				exec_pwd(void);
+int				execute(t_list *instr, t_shell_data *data);
+void			loop_shell(t_shell_data *data);
+void			free_path(char **path);
+char			**get_path(char **env);
+char			*check_access(char *command, char **path);
+char			*get_env_value(t_shell_data *data, char *key);
+
 
 /**** env functions ******/
 
@@ -51,32 +51,48 @@ int				remove_env_var(t_shell_data *data, int idx);
 
 /**** token functions ******/
 
-t_list		*lexer(char *command_str, t_shell_data *data);
-t_list		*lex_create_token(t_token_type type, char *value);
-void		lex_free_token_list(t_list **lst);
-void		append_last_token(t_list **tokens, char **cmd);
-void		append_var_name(t_list **tokens, char **cmd);
-void		substitute_env_var(t_list **tokens, t_shell_data *data);
+t_list			*lexer(char *command_str, t_shell_data *data);
+t_list			*lex_create_token(t_token_type type, char *value);
+void			lex_free_token_list(t_list **lst);
+void			append_last_token(t_list **tokens, char **cmd);
+void			append_var_name(t_list **tokens, char **cmd);
+void			substitute_env_var(t_list **tokens, t_shell_data *data);
 
-void		lex_quote_states(char **cmd, t_lex_st *st, t_list **res, char q);
-void		lex_var_state(char **cmd, t_lex_st *st, t_list **res, t_shell_data *data);
-void		lex_var_double_st(char **cmd, t_lex_st *st, t_list **l, t_shell_data *d);
-void		lex_word_state(char **cmd, t_lex_st *st, t_list **res);
-void		lex_start_state(char **command, t_lex_st *state, t_list **res);
-t_lex_st	lex_next_state(t_lex_st state, char command);
+void			lex_quote_states(char **cmd, t_lex_st *st,
+					t_list **res, char q);
+
+void			lex_var_state(char **cmd, t_lex_st *st,
+					t_list **res, t_shell_data *data);
+
+void			lex_var_double_st(char **cmd, t_lex_st *st,
+					t_list **l, t_shell_data *d);
+					
+void			lex_word_state(char **cmd, t_lex_st *st, t_list **res);
+void			lex_start_state(char **command, t_lex_st *state, t_list **res);
+t_lex_st		lex_next_state(t_lex_st state, char command);
 
 /**** parser functions ******/
 
 t_list			*parser(t_list *tokens);
 t_pars_st		pars_next_state(t_pars_st state, t_token *token);
-t_pars_err		pars_start_st(t_list *tokens, t_pars_st *state, t_list **commands);
-t_pars_err		pars_command_st(t_list *tokens, t_pars_st *state, t_list **commands);
-t_pars_err		pars_redirin_st(t_list *tokens, t_pars_st *state, t_list **commands);
-t_pars_err		pars_redirout_st(t_list *tokens, t_pars_st *state, t_list **commands);
-t_pars_err		pars_rediroappe_st(t_list *tokens, t_pars_st *state, t_list **commands);
+
+t_pars_err		pars_start_st(t_list *tokens, t_pars_st *state,
+					 t_list **commands);
+
+t_pars_err		pars_command_st(t_list *tokens, t_pars_st *state,
+					t_list **commands);
+
+t_pars_err		pars_redirin_st(t_list *tokens, t_pars_st *state, 
+					t_list **commands);
+
+t_pars_err		pars_redirout_st(t_list *tokens, t_pars_st *state, 
+					t_list **commands);
+
+t_pars_err		pars_rediroappe_st(t_list *tokens, t_pars_st *state,
+					t_list **commands);
+
 t_pars_err		pars_redheredoc_st(t_list *tokens, t_pars_st *state);
 t_pars_err		pars_invalid_st(t_list *tokens, t_pars_st *state);
-
 t_pars_err		pars_append_arg_to_command(t_list *commands, char *value);
 t_pars_err		pars_append_new_command(t_list **commands, char *value);
 void			pars_free_command_list(t_list **cmds);
@@ -86,8 +102,8 @@ t_pars_err		pars_set_stdout(t_list *commands, char *filename, int append);
 
 /**** builtin functions ******/
 
-int	built_in_env(t_command *command, t_shell_data *data);
-int	built_in_cd(t_command *instr, t_shell_data *data);
-
+int				built_in_env(t_command *command, t_shell_data *data);
+int				built_in_cd(t_command *instr, t_shell_data *data);
+int				exec_pwd(void);
 
 #endif
