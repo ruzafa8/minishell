b741e6259a59b2c94971ddafc9fb30d53bc9bffb
Alejandro jose Ruzafa casas <aruzafa-@c1r3s1.42malaga.com>
2023-06-04 21:01:06 +0200

    style(cd): norminette it

--- src/executer/exec_cd.c
+++ src/executer/exec_cd.c
@@ -1,81 +1,84 @@
-
-
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cd.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruzafa- <aruzafa-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/04 20:38:32 by aruzafa-          #+#    #+#             */
+/*   Updated: 2023/06/04 21:00:00 by aruzafa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-
 /**
- * change old pwd and new pwd with the one at the parameter 
+ * Change old pwd and new pwd with the one at the parameter 
  * if the "new" parameter is null, then swap oldpwd and pwd
-*/
+ */
 static void	update_pwds(t_shell_data *data, char *new)
 {
 	char	*aux;
 
-	if(new[0] == '\0') //caso de swap cuando hagas "cd -" o cosas asi
+	if (new[0] == '\0')
 	{
 		aux = ft_strdup(get_env_value(data, "OLDPWD"));
 		set_env_var(data, "OLDPWD", get_env_value(data, "PWD"));
 		set_env_var(data, "PWD", aux);
 		free(aux);
 	}
-	else //caso de cd normal
+	else
 	{
 		set_env_var(data, "OLDPWD", get_env_value(data, "PWD"));
 		set_env_var(data, "PWD", new);
 	}
 }
-/*
-	Posibles errores del cd:
 
-	Too many args,
-	The target might not exist, 
-	the target might not be a directory, 
-	you might not have permission to access the target directory
-*/
-int	built_in_cd(t_command *command, t_shell_data *data)
+static int	exec_cd(char *pathaux, t_shell_data *data)
 {
-	int	res_code;
-	char *pathaux;
-	char *dotdotcd;
+	int		res_code;
+	char	*new_path;
 
 	res_code = 0;
-	if (command->argc == 1 
-		|| (ft_strncmp(command->argv[1], "--", 3) == 0 && command->argc == 2)) //ir a home
+	if (chdir(pathaux) == -1)
+		res_code = errno;
+	if (res_code == 0)
+	{
+		new_path = getcwd(NULL, 0);
+		update_pwds(data, new_path);
+		free(new_path);
+	}
+	return (res_code);
+}
+
+/**
+ * Posibles errores del cd:
+ * 
+ * Too many args,
+ * The target might not exist, 
+ * the target might not be a directory, 
+ * you might not have permission to access the target directory
+ */
+int	built_in_cd(t_command *command, t_shell_data *data)
+{
+	int		res_code;
+	char	*pathaux;
+
+	res_code = 0;
+	if (command->argc == 1
+		|| (ft_strncmp(command->argv[1], "--", 3) == 0 && command->argc == 2))
 	{
 		pathaux = get_env_value(data, "HOME");
-		//if (!pathaux)
-		//	return (errorcomand(con la info));
-		if (chdir(pathaux) == -1)
-			res_code = errno;
-		if(res_code == 0) //si el cd va todo perfectisimo
-			update_pwds(data, pathaux);
+		res_code = exec_cd(pathaux, data);
 		free(pathaux);
 	}
-	else if (command->argc == 2 && ft_strncmp(command->argv[1], "..", 3) == 0) //volver patras
-	{
-		dotdotcd = get_env_value(data, "PWD");
-		if (chdir(command->argv[1]) == -1)
-			res_code = errno;
-		if(res_code == 0) //si el cd va todo perfectisimo
-		{
-			pathaux = getcwd(NULL, 0);//getcwd genera leaks?????
-			update_pwds(data, pathaux);
-			free(pathaux);
-		}
-		free(dotdotcd);
-	}
+	else if (command->argc == 2 && ft_strncmp(command->argv[1], "..", 3) == 0)
+		res_code = exec_cd(command->argv[1], data);
 	else if (command->argc == 2 && ft_strncmp(command->argv[1], ".", 2) == 0)
 		res_code = set_env_var(data, "OLDPWD", get_env_value(data, "PWD"));
-	else if (command->argc == 2) //cd clasic
-	{
-		if (chdir(command->argv[1]) == -1)
-			res_code = errno;
-		if(res_code == 0) //si el cd va todo perfectisimo
-			update_pwds(data, command->argv[1]);
-	}
-	else //cambiar porque es basura los errores por ahora
+	else if (command->argc == 2)
+		res_code = exec_cd(command->argv[1], data);
+	else
 		res_code = 1;
 	return (res_code);
 }
